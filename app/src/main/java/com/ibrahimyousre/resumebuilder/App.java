34249ae73b151d4203946e12f5e35481174cf89b
Ibrahim Yousre <ib.yousre@gmail.com>
2018-07-14 01:38:26 +0200

    Updating gradle and library versions Including firebase analytics and crashlytics

---
+++ app/src/main/java/com/ibrahimyousre/resumebuilder/App.java
@@ -0,0 +1,52 @@
+package com.ibrahimyousre.resumebuilder;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashlyticsCore core = new CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build();
+        Fabric.with(this, new Crashlytics.Builder().core(core).build());
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+        Timber.plant(new CrashlyticsTree());
+    }
+
+    private class CrashlyticsTree extends Timber.Tree {
+
+        private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
+        private static final String CRASHLYTICS_KEY_TAG = "tag";
+        private static final String CRASHLYTICS_KEY_MESSAGE = "message";
+
+        @Override
+        protected void log(int priority, String tag, @NonNull String message, Throwable t) {
+            if (priority <= Log.INFO) {
+                return;
+            }
+
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
+
+            if (t == null) {
+                Crashlytics.logException(new Exception(message));
+            } else {
+                Crashlytics.logException(t);
+            }
+        }
+    }
+}
